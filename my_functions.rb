ec6f38ab40ac320f434f581c134921c40f36ea8f
Thomas Gervolino <thomas_gervolino@hotmail.com>
2019-04-02 14:53:11 +0100

    fixed 3 errors

---
+++ my_functions.rb
@@ -0,0 +1,15 @@
+def greet(name, time_of_day)
+  return "Good #{time_of_day}, #{name.capitalize()}"
+end
+
+def return_10()
+  return 10
+end
+
+def add(number1, number2)
+  return number1 + number2
+end
+
+def subtract(num1, num2)
+  return num1 - num2
+end
