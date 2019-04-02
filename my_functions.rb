dc3e6369bd6666b483a6e906e6210c2e9adc0527
Thomas Gervolino <thomas_gervolino@hotmail.com>
2019-04-02 15:26:37 +0100

    solved numbber_of_months tests(3)

--- my_functions.rb
+++ my_functions.rb
@@ -33,3 +33,35 @@ end
 def add_string_as_number(str1, str2)
   return str1.to_i + str2.to_i
 end
+
+def number_to_full_month_name(num_month)
+  case num_month
+  when 1
+    month = "January"
+  when 2
+    month = "February"
+  when 3
+    month = "March"
+  when 4
+    month = "April"
+  when 5
+    month = "May"
+  when 6
+    month = "June"
+  when 7
+    month = "July"
+  when 8
+    month = "August"
+  when 9
+    month = "September"
+  when 10
+    month = "October"
+  when 11
+    month = "November"
+  when 12
+    month = "December"
+  esle
+    return "invalid"
+  end
+  return month
+end
