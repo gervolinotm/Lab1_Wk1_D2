204b1d6b8961c653760f76eed87339655353430c
Thomas Gervolino <thomas_gervolino@hotmail.com>
2019-04-02 14:58:37 +0100

    problem 5

--- my_functions.rb
+++ my_functions.rb
@@ -6,8 +6,8 @@ def return_10()
   return 10
 end
 
-def add(number1, number2)
-  return number1 + number2
+def add(num1, num2)
+  return num1 + num2
 end
 
 def subtract(num1, num2)
@@ -16,4 +16,8 @@ end
 
 def multiply(num1, num2)
   return num1 * num2
+end
+
+def divide(num1, num2)
+  return num1 / num2
 end
